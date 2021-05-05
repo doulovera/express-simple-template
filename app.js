aacbcdf9c084aac04a02a30674d1325b2afabe84
Chris <chris.glood3011@gmail.com>
2021-05-04 20:57:47 -0400

    Eliminando constante port y adaptandola a express

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 4000;
+app.use("port", process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
 });
 
-app.listen(port, () => {
-    console.log('Initiated')
-})
+app.listen(app.get("port"), () => {
+    console.log(`Server on port: ${port}`);
+});
