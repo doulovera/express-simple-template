51d27d02e3982739bac5be84480883db12103617
Chris <chris.glood3011@gmail.com>
2021-05-04 20:59:02 -0400

    Comentarios descriptivos

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
+// Requirements
 const express = require('express');
 const app = express();
-
+// Configuration
 app.use("port", process.env.PORT || 3000);
-
+// Routes
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
 });
-
+// Start server
 app.listen(app.get("port"), () => {
     console.log(`Server on port: ${port}`);
 });
