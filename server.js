418ee0c87ee8c59f55087a0804d3df7f58fd06ac
Chris <chris.glood3011@gmail.com>
2021-08-26 01:36:27 -0400

    Fixes and new packages

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 // Requirements
 const path = require("path")
-const express = require('express');
+const morgan = require("morgan");
+const express = require("express");
 const app = express();
 // Configuration
+app.use(morgan("dev"))
 app.use(express.static(path.join(__dirname, 'views')));
 app.set("port", process.env.PORT || 3000);
+// Template Engine
+app.set("views", path.join(__dirname, '/views'));
+app.set('view engine', 'pug');
 // Routes
 app.use(require('./routes/index'));
 // Start server
 app.listen(app.get("port"), () => {
-    console.log(`Server on port: ${app.get("port")}`);
+    console.log("Server on port:", app.get("port"));
 });
